c768d1d3e2418e9196e561123215008e22699207
Daniella Camilo <123254815+daniellacamilo@users.noreply.github.com>
2026-04-13 12:23:30 -0300

    Atualizaçao do App.tsx
    Refactored App component to simplify routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Footer from './components/footer/Footer'
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
 
 function App() {
 
 
   return (
     <>
-      <BrowserRouter>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <div className="flex flex-1 w-full">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
